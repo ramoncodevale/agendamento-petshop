4f1df16756e8829bd2d2c463560b275f1478c441
ramoncodevale <ramonvalentim88@gmail.com>
2025-07-22 14:27:58 -0300

    feat: visual adjustments to the AgendaManha component

--- src/components/agenda-manha.tsx
+++ src/components/agenda-manha.tsx
@@ -10,19 +10,17 @@ export const AgendaManha = () => {
           <p className="text-[#98959D] font-bold">09h-12h</p>
         </div>
 
-        <div className="w-full border mt-3 border-[#615656]" />
+        <div className="w-full border mt-3 border-[#353339]" />
 
-        <div className="flex justify-between items-center mt-9">
-          <div className="flex items-center gap-4">
-            <p className="text-white text-[14px] font-bold">09:00</p>
-            <p className="text-white text-[12px] font-semibold">
-              Thor / <span className="text-[#98959D] text-[12px]">Fernanda Costa</span>
-            </p>
-          </div>
-          <p className="text-[#98959D] text-[12px]">Vacinação</p>
-          <p className="text-[#666666] text-[12px]">Remover agendamento</p>
-        </div>
+       <div className="flex justify-between items-center mt-9 px-4">
+      <div className="flex items-center gap-4">
+        <p className="text-white  text-[14px] font-bold">09:00</p>
+        <p className="text-white text-[12px] font-semibold">Thor <span className="text-[#98959D] text-[12px]">/ Fernanda Costa</span></p>
       </div>
+      <p className="text-[#98959D] text-[12px]">Vacinação</p>
+      <p className="text-[#666666] text-[12px]">Remover agendamento</p>
+      </div>
+       </div>
     </section>
   );
 };
